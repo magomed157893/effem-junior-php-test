fea87c9d64798376b9d9aeaa90386d12084879ed
Magomed Aliev <magomed157893@gmail.com>
2025-08-23 20:33:11 +0300

    Finally set it up index.php

--- public/index.php
+++ public/index.php
@@ -4,4 +4,10 @@ define('BASE_PATH', dirname(__DIR__));
 
 require_once(BASE_PATH . '/vendor/autoload.php');
 
-echo "Hello World\n";
+use App\Utils\Request;
+use App\Utils\Router;
+
+$request = new Request();
+
+$router = new Router($request);
+$router->handle();
